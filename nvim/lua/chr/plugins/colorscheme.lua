b21a0ad37e9efe4b0ee02b312291ee184ed91448
Christian Höntsch-Rode <chr@MacMini.local>
2025-01-19 15:40:09 +0100

    feat: enable transparency.

--- nvim/lua/chr/plugins/colorscheme.lua
+++ nvim/lua/chr/plugins/colorscheme.lua
@@ -5,6 +5,8 @@ return {
   config = function()
     require("tokyonight").setup({
       style = "night",
+      transparent = "true",
+      styles = { sidebars = "transparent", floats = "transparent" },
     })
 
     vim.cmd("colorscheme tokyonight")
