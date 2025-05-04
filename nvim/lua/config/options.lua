03d70f9e3b1c07dd12b3f00ef842d500c3d06a53
U548230 <christian.hoentsch-rode@lhind.dlh.de>
2025-05-04 14:05:23 +0200

    feat(nvim): remove ruler

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- Add ruler
-vim.opt.colorcolumn = "80"
-vim.cmd([[highlight ColorColumn ctermbg=0 guibg=lightgrey]])
